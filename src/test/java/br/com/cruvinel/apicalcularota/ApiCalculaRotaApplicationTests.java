3937893be4a7026a5e0b0b0c71904de39dd98470
jaquecruvinel <jaquecruvinel@hotmail.com>
2021-05-04 21:06:51 -0300

    Primeiro commit

---
+++ src/test/java/br/com/cruvinel/apicalcularota/ApiCalculaRotaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cruvinel.apicalcularota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCalculaRotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
